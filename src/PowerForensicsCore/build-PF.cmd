a5b72ad8d8881e8677a4fdf4f0d5b0682a6718f5
Jared Atkinson <jared@invoke-ir.com>
2016-12-09 20:30:01 -0500

    Added auto build for PowerForensics Portable :-)

--- src/PowerForensicsCore/build-PF.cmd
+++ src/PowerForensicsCore/build-PF.cmd
@@ -3,4 +3,4 @@ move .\bin\Release\netstandard1.6\PowerForensics.dll ..\..\Modules\PowerForensic
 copy ..\..\Modules\PowerForensics\lib\coreclr\PowerForensics.dll ..\..\lib\coreclr
 copy ..\..\Modules\PowerForensics\lib\PSv2\PowerForensics.dll ..\..\lib\PSv2
 set CURRENTDIR=%cd%
-powershell.exe -ExecutionPolicy Unrestricted -File "%CURRENTDIR%\..\..\xmldoc2md\xmldoc2md.ps1" -ProjectPath "%CURRENTDIR%\..\.."
+powershell.exe -ExecutionPolicy Unrestricted -File "%CURRENTDIR%\..\..\build\build.ps1" -ProjectPath "%CURRENTDIR%\..\.."
